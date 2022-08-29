e8ac6135b56e0d3eb94c5cbe134a35765b60e500
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-08-29 20:51:51 +0600

    jwt verify

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,39 @@
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
-
 dotenv.config();
 // middleware
 const app: Express = express();
-
 app.use(cors()); /* NEW */
 app.use(express.json());
 
-
 const port = process.env.PORT || 5000;
+const jwtToken = process.env.ACCESS_TOKEN_SECRET || "";
+
+// verify jwt token
+function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const authorization = req.headers?.authorization;
+  // console.log(`authorization`, authorization);
+  if (!authorization) {
+    return res
+      .status(403)
+      .send({ success: false, message: "Forbidden Access" });
+  }
+  const token = authorization.split(" ")[1];
+  // console.log(`token`, token);
+  jwt.verify(token, jwtToken, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ success: false, message: "Forbidden Access" });
+    }
+    //  add the decoded user to the request object
+    req.body.user = decoded;    
+    next();
+  });
+}
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`
@@ -37,12 +59,14 @@ async function connect() {
   const userCollections = client.db("notes").collection("user");
 
   //   get api
-  app.get("/api/notes", async (req: Request, res: Response) => {
+  app.get("/api/notes", verifyToken, async (req: Request, res: Response) => {
     const notes = await noteCollections
       .find({})
       .sort({ $natural: -1 })
       .toArray();
     res.json(notes);
+    // console.log(req.body.user.email);
+    
   });
 
   // get api by id
@@ -72,6 +96,22 @@ async function connect() {
       _id: new ObjectId(req.params.id),
     });
     res.json(note);
+  });
+
+  // user put api
+  app.put("/api/user/:email", async (req, res) => {
+    const email = req.params.email;
+    // console.log(email);
+    const user = req.body;
+    const filter = { email: email };
+    const options = { upsert: true };
+    const updateDoc = {
+      $set: user,
+    };
+    const token = jwt.sign({ email: email }, jwtToken, { expiresIn: "1d" });
+    // console.log(token);
+    const result = await userCollections.updateOne(filter, updateDoc, options);
+    res.send({ result, token });
   });
 }
 
